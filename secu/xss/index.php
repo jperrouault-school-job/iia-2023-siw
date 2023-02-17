cbb5a472d4459bde3d5860303f89b267267ce7cb
Jeremy <jeremy.perrouault@ascadis.fr>
2023-02-17 11:30:44 +0100

    JOUR 2 - SECU - DEMO XSS 1

---
+++ secu/xss/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Démo XSS</title>
+</head>
+<body>
+    <?php
+    $username = "";
+
+    if (isset($_POST['username'])) {
+        $username = $_POST['username'];
+    }
+    ?>    
+
+    <form method="POST">
+        <input type="text" name="username" />
+        <input type="submit" value="OK" />
+    </form>
+
+    <h1>Bonjour <?= $username ?></h1>
+</body>
+</html>
